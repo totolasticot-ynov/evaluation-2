7daf229442a7adfb6ceb56df743185b6ec050708
dhthomas <thomas.dhordain@ynov.com>
2025-09-15 10:28:40 +0200

    evaluation2

---
+++ exercice2/main2.go
@@ -0,0 +1,5 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	var result string
+	for i := range args }
---
+++ exercice3/main3.go
@@ -0,0 +1,8 @@
+package piscine
+
+import "fmt"
+
+func SplitWhiteSpaces(s string) []string {
+	fmt.Println("           ")
+	return []string{}
+}
